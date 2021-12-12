bea2efec33540063c9c970cb9ca73666b2600650
Simon Gomes <simon.gomes@umma.id>
2021-12-12 23:37:11 +0600

    member data added

---
+++ src/utils/users.js
@@ -0,0 +1,287 @@
+export const USERS = [
+  {
+    gender: "female",
+    name: {
+      title: "Miss",
+      first: "Ninon",
+      last: "Caron",
+    },
+    location: {
+      street: {
+        number: 5286,
+        name: "Rue de L'Abbé-De-L'Épée",
+      },
+      city: "Tours",
+      state: "Haute-Corse",
+      country: "France",
+      postcode: 48941,
+      coordinates: {
+        latitude: "-3.8044",
+        longitude: "121.4429",
+      },
+      timezone: {
+        offset: "-9:00",
+        description: "Alaska",
+      },
+    },
+    email: "ninon.caron@example.com",
+    login: {
+      uuid: "d21188f5-6d73-4b8c-b3ba-a358f9123271",
+      username: "purplemouse324",
+      password: "redsox1",
+      salt: "ZBnTFVUl",
+      md5: "f43eeac686a7c9739e955e72af366c5e",
+      sha1: "1e0775cfa504d9dc84e8b6584aaabb121dde7318",
+      sha256:
+        "9608192a4097481216898ee1fc3d7713efb7518fc938774ed13be275c4609c04",
+    },
+    dob: {
+      date: "1961-10-26T20:49:41.438Z",
+      age: 60,
+    },
+    registered: {
+      date: "2011-11-24T08:00:41.928Z",
+      age: 10,
+    },
+    phone: "04-11-21-40-64",
+    cell: "06-72-91-70-27",
+    id: {
+      name: "INSEE",
+      value: "2NNaN74890606 12",
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/women/94.jpg",
+      medium: "https://randomuser.me/api/portraits/med/women/94.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/women/94.jpg",
+    },
+    nat: "FR",
+  },
+  {
+    gender: "female",
+    name: {
+      title: "Ms",
+      first: "Emily",
+      last: "Pelletier",
+    },
+    location: {
+      street: {
+        number: 7411,
+        name: "Grand Ave",
+      },
+      city: "Hudson",
+      state: "Northwest Territories",
+      country: "Canada",
+      postcode: "W6U 3L9",
+      coordinates: {
+        latitude: "29.8736",
+        longitude: "-1.0374",
+      },
+      timezone: {
+        offset: "+8:00",
+        description: "Beijing, Perth, Singapore, Hong Kong",
+      },
+    },
+    email: "emily.pelletier@example.com",
+    login: {
+      uuid: "b4e2c14b-b6ef-4191-a252-e6cadb866d35",
+      username: "goldenkoala316",
+      password: "carmex2",
+      salt: "nwkiFPWl",
+      md5: "36905b2d5c9b5a50d20a782cd8becdbf",
+      sha1: "9713cb89568f732b67c922c80874f9d32dbc476f",
+      sha256:
+        "8cb4e201355be6d69ce9485a8c4160a3cef2a711c6a4fdeea6a9c17267c26879",
+    },
+    dob: {
+      date: "1984-10-18T12:22:15.544Z",
+      age: 37,
+    },
+    registered: {
+      date: "2007-01-17T03:03:13.804Z",
+      age: 14,
+    },
+    phone: "883-009-3153",
+    cell: "167-914-0454",
+    id: {
+      name: "",
+      value: null,
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/women/46.jpg",
+      medium: "https://randomuser.me/api/portraits/med/women/46.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/women/46.jpg",
+    },
+    nat: "CA",
+  },
+  {
+    gender: "male",
+    name: {
+      title: "Mr",
+      first: "Coşkun",
+      last: "Koçyiğit",
+    },
+    location: {
+      street: {
+        number: 8047,
+        name: "Şehitler Cd",
+      },
+      city: "Balıkesir",
+      state: "Karabük",
+      country: "Turkey",
+      postcode: 91043,
+      coordinates: {
+        latitude: "-58.1966",
+        longitude: "-131.1590",
+      },
+      timezone: {
+        offset: "+5:30",
+        description: "Bombay, Calcutta, Madras, New Delhi",
+      },
+    },
+    email: "coskun.kocyigit@example.com",
+    login: {
+      uuid: "8ac23463-b47c-4c51-9e52-842bbf170796",
+      username: "bluemouse752",
+      password: "thunder1",
+      salt: "Lssifhxn",
+      md5: "d74af439ca6489588694f02d33b4ba64",
+      sha1: "2ebf34b6dcf15e8db162f52f878ee6a7ddb2f3e0",
+      sha256:
+        "f43a66039b747388ebe35b600575dd829439cdd0e1e030a83c050c586a6ea03b",
+    },
+    dob: {
+      date: "1959-10-26T20:07:23.170Z",
+      age: 62,
+    },
+    registered: {
+      date: "2004-09-12T14:40:33.880Z",
+      age: 17,
+    },
+    phone: "(434)-783-6302",
+    cell: "(217)-211-0444",
+    id: {
+      name: "",
+      value: null,
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/men/94.jpg",
+      medium: "https://randomuser.me/api/portraits/med/men/94.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/men/94.jpg",
+    },
+    nat: "TR",
+  },
+  {
+    gender: "female",
+    name: {
+      title: "Mrs",
+      first: "Frida",
+      last: "Madsen",
+    },
+    location: {
+      street: {
+        number: 9904,
+        name: "Skovgårdsvej",
+      },
+      city: "Gl. Rye",
+      state: "Sjælland",
+      country: "Denmark",
+      postcode: 45436,
+      coordinates: {
+        latitude: "65.3775",
+        longitude: "62.6126",
+      },
+      timezone: {
+        offset: "-3:30",
+        description: "Newfoundland",
+      },
+    },
+    email: "frida.madsen@example.com",
+    login: {
+      uuid: "c57ee228-bc51-4fe9-8421-163e3ab42f75",
+      username: "whitepanda830",
+      password: "dieter",
+      salt: "pllG1aXZ",
+      md5: "f2810b6fde5ff41bf2c05e921aec7129",
+      sha1: "122c6d9742285d5e350a93fcb12dd175b7bc75df",
+      sha256:
+        "a71a7975627c573fc7c91daa973841b24db420d01a7c4ac0172429596e17d5d4",
+    },
+    dob: {
+      date: "1980-09-13T11:00:53.106Z",
+      age: 41,
+    },
+    registered: {
+      date: "2002-09-06T07:30:13.782Z",
+      age: 19,
+    },
+    phone: "36475176",
+    cell: "89741985",
+    id: {
+      name: "CPR",
+      value: "130980-5103",
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/women/89.jpg",
+      medium: "https://randomuser.me/api/portraits/med/women/89.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/women/89.jpg",
+    },
+    nat: "DK",
+  },
+  {
+    gender: "female",
+    name: {
+      title: "Miss",
+      first: "Purificacion",
+      last: "Iglesias",
+    },
+    location: {
+      street: {
+        number: 1301,
+        name: "Paseo de Extremadura",
+      },
+      city: "Ciudad Real",
+      state: "Comunidad Valenciana",
+      country: "Spain",
+      postcode: 54043,
+      coordinates: {
+        latitude: "-3.9136",
+        longitude: "83.6059",
+      },
+      timezone: {
+        offset: "+11:00",
+        description: "Magadan, Solomon Islands, New Caledonia",
+      },
+    },
+    email: "purificacion.iglesias@example.com",
+    login: {
+      uuid: "e6940332-b322-4077-b4d5-794fb3b63338",
+      username: "bigleopard679",
+      password: "deville",
+      salt: "UZ3iVtAk",
+      md5: "bb5eb83f64353079e5dcd4d2a31b25e1",
+      sha1: "0179022666d58f0957743d1ecfad04d3ee6403c7",
+      sha256:
+        "c0cec1b37da63d7f09e41ef2fae0116b6986d672ed1e7eceb5a367a6545bd910",
+    },
+    dob: {
+      date: "1984-03-12T22:48:56.181Z",
+      age: 37,
+    },
+    registered: {
+      date: "2017-04-04T03:55:39.069Z",
+      age: 4,
+    },
+    phone: "943-111-252",
+    cell: "646-597-996",
+    id: {
+      name: "DNI",
+      value: "34088599-H",
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/women/38.jpg",
+      medium: "https://randomuser.me/api/portraits/med/women/38.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/women/38.jpg",
+    },
+    nat: "ES",
+  },
+];
